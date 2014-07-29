391a335bed88798170dcdf0db13f1494bc1d7297
Julien Danjou <julien@danjou.info>
2014-07-29 13:45:52 +0200

    Allow up to 100 connections in PG for tests
    Change-Id: Idc7c3d993196b5ea60160f971a8b76b1da9857b7

--- run-postgresql-tests.sh
+++ run-postgresql-tests.sh
@@ -15,7 +15,7 @@ PGSQL_DATA=`mktemp -d /tmp/gnocchi-psql-XXXXX`
 PGSQL_PATH=`pg_config --bindir`
 ${PGSQL_PATH}/initdb ${PGSQL_DATA}
 mkfifo ${PGSQL_DATA}/out
-${PGSQL_PATH}/postgres -F -k ${PGSQL_DATA} -D ${PGSQL_DATA} -p 9823 &> ${PGSQL_DATA}/out &
+${PGSQL_PATH}/postgres -N 100 -F -k ${PGSQL_DATA} -D ${PGSQL_DATA} -p 9823 &> ${PGSQL_DATA}/out &
 # Wait for PostgreSQL to start listening to connections
 wait_for_line "database system is ready to accept connections" ${PGSQL_DATA}/out
 export GNOCCHI_TEST_PGSQL_URL="postgresql:///?host=${PGSQL_DATA}&port=9823&dbname=template1"
