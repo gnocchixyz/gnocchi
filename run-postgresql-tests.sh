ea65678096a7c881d2da808daf8809c31f6caf13
Julien Danjou <julien@danjou.info>
2014-11-10 11:41:37 +0100

    postgresql: redirect logs to /dev/null
    Change-Id: Id1055203457d019fdce3dbd44123ead4852caa81

--- run-postgresql-tests.sh
+++ run-postgresql-tests.sh
@@ -5,7 +5,7 @@ PGSQL_DATA=`mktemp -d /tmp/gnocchi-psql-XXXXX`
 PGSQL_PATH=`pg_config --bindir`
 PGSQL_PORT=9823
 ${PGSQL_PATH}/initdb ${PGSQL_DATA}
-LANGUAGE=C ${PGSQL_PATH}/pg_ctl -w -D ${PGSQL_DATA} -o "-k ${PGSQL_DATA} -p ${PGSQL_PORT}" start
+LANGUAGE=C ${PGSQL_PATH}/pg_ctl -w -D ${PGSQL_DATA} -o "-k ${PGSQL_DATA} -p ${PGSQL_PORT}" start > /dev/null
 export GNOCCHI_TEST_PGSQL_URL="postgresql:///?host=${PGSQL_DATA}&port=${PGSQL_PORT}&dbname=template1"
 
 python setup.py testr --slowest --testr-args="$*"
