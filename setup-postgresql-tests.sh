37cc1bff64c3c0bf4d4121d03601b079926f7cf7
Julien Danjou <julien@danjou.info>
2014-12-02 16:59:58 +0100

    tests: use SQL scripts to setup env and run any command
    Change-Id: I3a21775bc3879bf873f5dcfe3b4108d0b1a94e32

--- setup-postgresql-tests.sh
+++ setup-postgresql-tests.sh
@@ -8,7 +8,7 @@ ${PGSQL_PATH}/initdb ${PGSQL_DATA}
 LANGUAGE=C ${PGSQL_PATH}/pg_ctl -w -D ${PGSQL_DATA} -o "-k ${PGSQL_DATA} -p ${PGSQL_PORT}" start > /dev/null
 export GNOCCHI_TEST_PGSQL_URL="postgresql:///?host=${PGSQL_DATA}&port=${PGSQL_PORT}&dbname=template1"
 
-python setup.py testr --slowest --testr-args="$*"
+$*
 
 ret=$?
 ${PGSQL_PATH}/pg_ctl -w -D ${PGSQL_DATA} -o "-p $PGSQL_PORT" stop
