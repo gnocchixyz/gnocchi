01fb4f0af450a07619c26a72011c9a564b8b0bd7
Chris Dent <chdent@redhat.com>
2015-01-20 13:54:02 +0000

    Improve database handling in tests
    There are two main changes here:
    
    * The startup script for mysql will now work if there is an existing
      my.cnf that may be conflicting with the options set in the script.
      --no-defaults basically says "don't use my config".
    
    * Database creation is moved into the base test class such that each
      process during a concurrent test run can be using a different
      database. The base name of the database come from the setup
      scripts. Within the test process a uuid is appended to this to
      create a unique database name.
    
    Change-Id: I6c3bd1882e513a4b5ae738365a90a2ff26f4b3cf

--- setup-postgresql-tests.sh
+++ setup-postgresql-tests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
 # Start PostgreSQL process for tests
+DATABASE_BASENAME=test
 PGSQL_DATA=`mktemp -d /tmp/gnocchi-psql-XXXXX`
 PGSQL_PATH=`pg_config --bindir`
 PGSQL_PORT=9823
 ${PGSQL_PATH}/initdb ${PGSQL_DATA}
 LANGUAGE=C ${PGSQL_PATH}/pg_ctl -w -D ${PGSQL_DATA} -o "-k ${PGSQL_DATA} -p ${PGSQL_PORT}" start > /dev/null
-export GNOCCHI_TEST_PGSQL_URL="postgresql:///?host=${PGSQL_DATA}&port=${PGSQL_PORT}&dbname=template1"
+export GNOCCHI_TEST_PGSQL_URL="postgresql:///${DATABASE_BASENAME}?host=${PGSQL_DATA}&port=${PGSQL_PORT}"
 
 $*
 
