bc2fc929d6860e6f0b0d2fce6b834e5e9a0445e5
Julien Danjou <julien@danjou.info>
2015-05-21 17:21:26 -0700

    Switch to PyMySQL and enable MySQL on py34
    Change-Id: I1f681df9a4191131d1fa5aaaba9d2ccf07eeab2b

--- setup-mysql-tests.sh
+++ setup-mysql-tests.sh
@@ -15,7 +15,7 @@ PATH=$PATH:/usr/libexec
 mysqld --no-defaults --datadir=${MYSQL_DATA} --pid-file=${MYSQL_DATA}/mysql.pid --socket=${MYSQL_DATA}/mysql.socket --skip-networking --skip-grant-tables &> ${MYSQL_DATA}/out &
 # Wait for MySQL to start listening to connections
 wait_for_line "mysqld: ready for connections." ${MYSQL_DATA}/out
-export GNOCCHI_TEST_INDEXER_URL="mysql://root@localhost/test?unix_socket=${MYSQL_DATA}/mysql.socket&charset=utf8"
+export GNOCCHI_TEST_INDEXER_URL="mysql+pymysql://root@localhost/test?unix_socket=${MYSQL_DATA}/mysql.socket&charset=utf8"
 mysql --no-defaults -S ${MYSQL_DATA}/mysql.socket -e 'CREATE DATABASE test;'
 
 mkdir $MYSQL_DATA/tooz
