fcae40aaf29edd0fb95d61f097478dc2fd0ced23
Chris Dent <chdent@redhat.com>
2015-06-19 13:43:46 +0000

    Use a more unique postgresql port
    If you try to run the gnocchi postgresql tests at the same time as
    ceilometer then things break.
    
    A better solution would be to seek for an unused port but that's
    overkill at this point.
    
    Change-Id: Id57851aefa9e1b0f95b172d3facdad5447641c1f

--- setup-postgresql-tests.sh
+++ setup-postgresql-tests.sh
@@ -3,7 +3,7 @@
 # Start PostgreSQL process for tests
 PGSQL_DATA=`mktemp -d /tmp/gnocchi-psql-XXXXX`
 PGSQL_PATH=`pg_config --bindir`
-PGSQL_PORT=9823
+PGSQL_PORT=9824
 ${PGSQL_PATH}/pg_ctl initdb -D ${PGSQL_DATA}
 LANGUAGE=C ${PGSQL_PATH}/pg_ctl -w -D ${PGSQL_DATA} -o "-k ${PGSQL_DATA} -p ${PGSQL_PORT}" start > /dev/null
 export GNOCCHI_TEST_INDEXER_URL="postgresql:///template1?host=${PGSQL_DATA}&port=${PGSQL_PORT}"
