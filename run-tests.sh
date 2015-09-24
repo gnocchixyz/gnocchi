0357ea6c28ee328e8a4ce84480f3c0830f62cbb9
Julien Danjou <julien@danjou.info>
2015-09-24 16:51:04 +0200

    tox: fix too much test running in specific envs
    When running a specific env such as e.g. py27-postgresql-file, the lines
    in the global commands variable starting with "py27:" also matches,
    which means we ended up running the global test suite too!
    
    This refactor the code a bit so we always call run-tests.sh with just
    some environment variable set using `setenv'.
    
    Change-Id: Icb2879f383c1d0a09a1cd1fce1cbadfa1207c6c9

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x
 set -e
-EXIT_CODE=0
-INDEXER_DRIVERS="$1"
-shift
-STORAGE_DRIVERS="$1"
-shift
-for storage in $STORAGE_DRIVERS
+GNOCCHI_TEST_STORAGE_DRIVERS=${GNOCCHI_TEST_STORAGE_DRIVERS:-file}
+GNOCCHI_TEST_INDEXER_DRIVERS=${GNOCCHI_TEST_INDEXER_DRIVERS:-postgresql}
+for storage in ${GNOCCHI_TEST_STORAGE_DRIVERS}
 do
-    for indexer in $INDEXER_DRIVERS
+    for indexer in ${GNOCCHI_TEST_INDEXER_DRIVERS}
     do
-        GNOCCHI_TEST_STORAGE_DRIVER=$storage ./setup-${indexer}-tests.sh ./tools/pretty_tox.sh $*
+        storage_setup_script=./setup-${storage}-tests.sh
+        if [ ! -x "$storage_setup_script" ]
+        then
+            unset storage_setup_script
+        fi
+        GNOCCHI_TEST_STORAGE_DRIVER=$storage ./setup-${indexer}-tests.sh "${storage_setup_script}" ./tools/pretty_tox.sh $*
     done
 done
