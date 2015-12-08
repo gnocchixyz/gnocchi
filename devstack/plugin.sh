2dbd0e2c6e1ed9a859345ee0226595098138f0cb
Sumant Murke <sumant.murke@intel.com>
2015-12-08 22:02:45 +0000

    Resolving use of deprecated dispatcher configuration
    Updated plugin.sh file by resolving configuration changes for
    meter_dispatchers in devstack.
    
    Change-Id: Icb6bb613eb5cdad98785e8a176f44d78233cf12b
    Closes-Bug: 1524072

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -296,7 +296,7 @@ function configure_ceph_gnocchi {
 
 function configure_ceilometer_gnocchi {
     gnocchi_url=$(gnocchi_service_url)
-    iniset $CEILOMETER_CONF DEFAULT dispatcher gnocchi
+    iniset $CEILOMETER_CONF DEFAULT meter_dispatchers gnocchi
     iniset $CEILOMETER_CONF alarms gnocchi_url $gnocchi_url
     iniset $CEILOMETER_CONF dispatcher_gnocchi url $gnocchi_url
     iniset $CEILOMETER_CONF dispatcher_gnocchi archive_policy ${GNOCCHI_ARCHIVE_POLICY}
