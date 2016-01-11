f949c5c51dcbe9e0727e5e3fc0855f54d4bbe860
Julien Danjou <julien@danjou.info>
2016-01-11 15:52:02 +0100

    Use overtest to setup InfluxDB
    Change-Id: Iaf046e48ec4331911f5a142f82e26667a0ca8e37

--- run-tests.sh
+++ run-tests.sh
@@ -6,12 +6,8 @@ for storage in ${GNOCCHI_TEST_STORAGE_DRIVERS}
 do
     for indexer in ${GNOCCHI_TEST_INDEXER_DRIVERS}
     do
-        storage_setup_script=./setup-${storage}-tests.sh
-        if [ ! -x "$storage_setup_script" ]
-        then
-            unset storage_setup_script
-        fi
         export GNOCCHI_TEST_INDEXER_DRIVER=$indexer
-        GNOCCHI_TEST_STORAGE_DRIVER=$storage ./setup-test-env.sh ${storage_setup_script} ./tools/pretty_tox.sh $*
+        export GNOCCHI_TEST_STORAGE_DRIVER=$storage
+        ./setup-test-env.sh ./tools/pretty_tox.sh $*
     done
 done
--- setup-test-env.sh
+++ setup-test-env.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 set -x
+# Activate overtest for indexer
 GNOCCHI_TEST_INDEXER_DRIVER=${GNOCCHI_TEST_INDEXER_DRIVER:-postgresql}
 source $(which overtest) $GNOCCHI_TEST_INDEXER_DRIVER
 export GNOCCHI_INDEXER_URL=${OVERTEST_URL/#mysql:/mysql+pymysql:}
 export GNOCCHI_COORDINATION_URL=${OVERTEST_URL}
+# Activate overtest for storage
+case $GNOCCHI_TEST_STORAGE_DRIVER in
+    influxdb)
+        source $(which overtest) $GNOCCHI_TEST_STORAGE_DRIVER
+        GNOCCHI_TEST_INFLUXDB_PORT=${OVERTEST_INFLUXDB_PORT}
+        ;;
+    *)
+        ;;
+esac
 $*
