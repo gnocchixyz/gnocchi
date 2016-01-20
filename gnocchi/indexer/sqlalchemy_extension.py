e5bd6595b0624b8d9a3e62952a73a8137be321af
Mehdi Abaakouk <sileht@redhat.com>
2016-01-20 09:45:18 +0100

    Implements resource attribute number
    This change allows to create resource attributes of type number
    
    Blueprint resource-type-rest-api
    Change-Id: I3ada1bc8055fe63253bbf2c329ddbe6437f29d3f

--- gnocchi/indexer/sqlalchemy_extension.py
+++ gnocchi/indexer/sqlalchemy_extension.py
@@ -77,3 +77,7 @@ class StringSchema(resource_type.StringSchema):
 
 class UUIDSchema(resource_type.UUIDSchema):
     satype = sqlalchemy_utils.UUIDType()
+
+
+class NumberSchema(resource_type.NumberSchema):
+    satype = sqlalchemy.Float(53)
