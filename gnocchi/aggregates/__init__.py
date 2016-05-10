a1d02ee90979fdc0d060e2f14e663f149859c3f0
Julien Danjou <julien@danjou.info>
2016-05-10 17:09:20 +0200

    Update hacking to 0.11
    Change-Id: I4b79b56317e333cf12a94fae9dd8ec3bbfa020b5

--- gnocchi/aggregates/__init__.py
+++ gnocchi/aggregates/__init__.py
@@ -2,8 +2,6 @@
 #
 # Copyright 2014 OpenStack Foundation
 #
-# Authors: Ana Malagon  <atmalagon@gmail.com>
-#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
