6449ed81ea84a1d77d88707de0d1940c277cf15c
Julien Danjou <julien@danjou.info>
2017-01-03 14:07:19 +0100

    run-tests: use case rather than if/elif/else
    Change-Id: I4ec57a2ff3057ee2fe3ac49cab66cfe8628ab2d6

--- run-tests.sh
+++ run-tests.sh
@@ -7,23 +7,25 @@ do
     export GNOCCHI_TEST_STORAGE_DRIVER=$storage
     for indexer in ${GNOCCHI_TEST_INDEXER_DRIVERS}
     do
-        if [ "$GNOCCHI_TEST_STORAGE_DRIVER" == "ceph" ]; then
-            pifpaf run ceph -- pifpaf -g GNOCCHI_INDEXER_URL run $indexer -- ./tools/pretty_tox.sh $*
-        elif [ "$GNOCCHI_TEST_STORAGE_DRIVER" == "s3" ]
-        then
-            if ! which s3rver >/dev/null 2>&1
-            then
-                mkdir npm-s3rver
-                export NPM_CONFIG_PREFIX=npm-s3rver
-                npm install s3rver --global
-                export PATH=$PWD/npm-s3rver/bin:$PATH
-            fi
-            pifpaf -e GNOCCHI_STORAGE run s3rver -- \
-                   pifpaf -e GNOCCHI_INDEXER run $indexer -- \
-                   ./tools/pretty_tox.sh $*
-
-        else
-            pifpaf -g GNOCCHI_INDEXER_URL run $indexer -- ./tools/pretty_tox.sh $*
-        fi
+        case $GNOCCHI_TEST_STORAGE_DRIVER in
+            ceph)
+                pifpaf run ceph -- pifpaf -g GNOCCHI_INDEXER_URL run $indexer -- ./tools/pretty_tox.sh $*
+                ;;
+            s3)
+                if ! which s3rver >/dev/null 2>&1
+                then
+                    mkdir npm-s3rver
+                    export NPM_CONFIG_PREFIX=npm-s3rver
+                    npm install s3rver --global
+                    export PATH=$PWD/npm-s3rver/bin:$PATH
+                fi
+                pifpaf -e GNOCCHI_STORAGE run s3rver -- \
+                       pifpaf -e GNOCCHI_INDEXER run $indexer -- \
+                       ./tools/pretty_tox.sh $*
+                ;;
+            *)
+                pifpaf -g GNOCCHI_INDEXER_URL run $indexer -- ./tools/pretty_tox.sh $*
+                ;;
+        esac
     done
 done
