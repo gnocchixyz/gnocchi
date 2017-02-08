88cd1fc48096c5ecdf40ad9ced145285dab978e4
gord chung <gord@live.ca>
2017-02-08 18:45:34 +0000

    test non-uuid and add debug for upgrade gate
    - show the output of dump to help debug
    - ensure non-uuids from v2.2 are valid
    - ensure statsd resource gets upgraded
    
    Change-Id: Ic31af11fcf542c8309052ee75b554aa15e8604c3

--- run-upgrade-tests.sh
+++ run-upgrade-tests.sh
@@ -13,6 +13,7 @@ RESOURCE_IDS=(
     "5a301761-aaaa-46e2-8900-8b4f6fe6675a"
     "5a301761-bbbb-46e2-8900-8b4f6fe6675a"
     "5a301761-cccc-46e2-8900-8b4f6fe6675a"
+    "non-uuid"
 )
 
 [ "$have_resource_type_post" ] && RESOURCE_ID_EXT="5a301761/dddd/46e2/8900/8b4f6fe6675a"
@@ -21,7 +22,7 @@ dump_data(){
     dir="$1"
     mkdir -p $dir
     echo "* Dumping measures aggregations to $dir"
-    gnocchi resource list -c id -c type -c project_id -c user_id -c original_resource_id -c started_at -c ended_at -c revision_start -c revision_end > $dir/resources.list
+    gnocchi resource list -c id -c type -c project_id -c user_id -c original_resource_id -c started_at -c ended_at -c revision_start -c revision_end | tee $dir/resources.list
     for resource_id in ${RESOURCE_IDS[@]} $RESOURCE_ID_EXT; do
         for agg in min max mean sum ; do
             gnocchi measures show --aggregation $agg --resource-id $resource_id metric > $dir/${agg}.txt
@@ -87,7 +88,7 @@ eval $(pifpaf run gnocchi --indexer-url $INDEXER_URL --storage-url $STORAGE_URL)
 export OS_AUTH_TYPE=gnocchi-noauth
 export GNOCCHI_USER_ID=admin
 export GNOCCHI_PROJECT_ID=admin
-gnocchi resource delete $GNOCCHI_STATSD_RESOURCE_ID
+original_statsd_resource_id=$GNOCCHI_STATSD_RESOURCE_ID
 inject_data $GNOCCHI_DATA
 # Encode resource id as it contains slashes and gnocchiclient does not encode it
 [ "$have_resource_type_post" ] && RESOURCE_ID_EXT="19235bb9-35ca-5f55-b7db-165cfb033c86"
@@ -103,12 +104,14 @@ eval $(pifpaf --debug run gnocchi --indexer-url $INDEXER_URL --storage-url $STOR
 export OS_AUTH_TYPE=gnocchi-basic
 export GNOCCHI_USER=$GNOCCHI_USER_ID
 
+# pifpaf creates a new statsd resource on each start
 gnocchi resource delete $GNOCCHI_STATSD_RESOURCE_ID
 
 RESOURCE_IDS=(
     "5a301761-aaaa-46e2-8900-8b4f6fe6675a"
     "5a301761-bbbb-46e2-8900-8b4f6fe6675a"
     "5a301761-cccc-46e2-8900-8b4f6fe6675a"
+    "24d2e3ed-c7c1-550f-8232-56c48809a6d4"
 )
 # NOTE(sileht): / are now _
 # NOTE(jdanjou): and we reencode for admin:admin, but we cannot authenticate as
@@ -120,7 +123,9 @@ dump_data $GNOCCHI_DATA/new
 
 # NOTE(sileht): change the output of the old gnocchi to compare with the new without '/'
 $GSED -i -e "s,5a301761/dddd/46e2/8900/8b4f6fe6675a,5a301761_dddd_46e2_8900_8b4f6fe6675a,g" \
-      -e "s,19235bb9-35ca-5f55-b7db-165cfb033c86,517920a9-2e50-58b8-88e8-25fd7aae1d8f,g" $GNOCCHI_DATA/old/resources.list
+      -e "s,19235bb9-35ca-5f55-b7db-165cfb033c86,517920a9-2e50-58b8-88e8-25fd7aae1d8f,g" \
+      -e "s,None                                ,${original_statsd_resource_id},g" \
+      -e "s,37d1416a-381a-5b6c-99ef-37d89d95f1e1,24d2e3ed-c7c1-550f-8232-56c48809a6d4,g" $GNOCCHI_DATA/old/resources.list
 
 echo "* Checking output difference between Gnocchi $old_version and $new_version"
 diff -uNr $GNOCCHI_DATA/old $GNOCCHI_DATA/new
