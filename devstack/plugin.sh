6377e25bdcca68be66fadf65aa16a6f174cfaa99
Mehdi Abaakouk <sileht@redhat.com>
2017-05-18 07:37:14 +0200

    wsgi: use pbr wsgi binary
    We don't need to provide the app.wsgi script anymore.
    pbr provides it within the gnocchi-api binary
    
    Change-Id: I6e16607128849a18b9e6eb1bc5558d1c9df64775

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -281,7 +281,7 @@ function configure_gnocchi {
         rm -f "$GNOCCHI_UWSGI_FILE"
 
         iniset "$GNOCCHI_UWSGI_FILE" uwsgi http $GNOCCHI_SERVICE_HOST:$GNOCCHI_SERVICE_PORT
-        iniset "$GNOCCHI_UWSGI_FILE" uwsgi wsgi-file "$GNOCCHI_DIR/gnocchi/rest/app.wsgi"
+        iniset "$GNOCCHI_UWSGI_FILE" uwsgi wsgi-file "/usr/local/bin/gnocchi-api"
         # This is running standalone
         iniset "$GNOCCHI_UWSGI_FILE" uwsgi master true
         # Set die-on-term & exit-on-reload so that uwsgi shuts down
