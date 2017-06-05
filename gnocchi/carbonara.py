0b0c2289282062bc0a6c53e669fa0d30f83d4285
Julien Danjou <julien@danjou.info>
2017-06-05 22:24:20 +0200

    storage: fix resample on empty metric
    If a metric is empty, carbonara raises an IndexError when checking for the data
    on grouping since the Pandas timeseries is empty.
    
    Fixes #69
    
    (cherry picked from commit 5fb48769440cc405d375d7721fecf82e0dd5610f)

--- gnocchi/carbonara.py
+++ gnocchi/carbonara.py
@@ -245,7 +245,7 @@ class TimeSerie(object):
         # NOTE(jd) Our whole serialization system is based on Epoch, and we
         # store unsigned integer, so we can't store anything before Epoch.
         # Sorry!
-        if self.ts.index[0].value < 0:
+        if not self.ts.empty and self.ts.index[0].value < 0:
             raise BeforeEpochError(self.ts.index[0])
 
         return GroupedTimeSeries(self.ts[start:], granularity)
