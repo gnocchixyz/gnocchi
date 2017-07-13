5103dc82b357e814ccd7248435ed247a2e04ced6
Julien Danjou <julien@danjou.info>
2017-07-13 16:47:22 +0200

    Only retry connection to external components in metricd
    Currently storage such as Swift will retry forever to connect on __init__.
    Actually, the only process that wants to retry indefinitely is metricd. The API
    will keep the client connected forever if it does not raise a 500 error soon
    enough.
    
    This patches make sure that the only part retrying for ever is metricd: the
    rest (e.g. the API) will fail fast if anything bad happens.
    
    Fixes #194

--- gnocchi/utils.py
+++ gnocchi/utils.py
@@ -29,7 +29,6 @@ import numpy
 import pandas as pd
 import six
 from stevedore import driver
-import tenacity
 from tooz import coordination
 
 
@@ -71,27 +70,9 @@ def UUID(value):
         raise ValueError(e)
 
 
-# Retry with exponential backoff for up to 1 minute
-retry = tenacity.retry(
-    wait=tenacity.wait_exponential(multiplier=0.5, max=60),
-    # Never retry except when explicitly asked by raising TryAgain
-    retry=tenacity.retry_never,
-    reraise=True)
-
-
-# TODO(jd) Move this to tooz?
-@retry
-def _enable_coordination(coord):
-    try:
-        coord.start(start_heart=True)
-    except Exception as e:
-        LOG.error("Unable to start coordinator: %s", e)
-        raise tenacity.TryAgain(e)
-
-
 def get_coordinator_and_start(url):
     coord = coordination.get_coordinator(url, str(uuid.uuid4()).encode())
-    _enable_coordination(coord)
+    coord.start(start_heart=True)
     return coord
 
 
