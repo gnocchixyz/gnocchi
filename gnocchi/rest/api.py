45d7864a93a9cbbf9c49ec518cde427a4dcf8f41
Mehdi Abaakouk <sileht@redhat.com>
2017-11-10 18:56:49 +0100

    aggregates: rework API output
    This design have some issue when two timeseries have the same names,
    only the last one is dumped. Or if the same metric is asked
    twice for whatever reason, measures have all timestamps duplicated.
    
    This change the result to a new format:
    
    For resources:
    {
      "measures": { resource_id: {metric_name: {aggregation: MEASURES}}},
      "references": [RESOURCES_DETAILS]
    }
    
    For metrics
    {
      "measures": {metric_id: {aggregation: MEASURES}},
      "references": [METRICS_DETAILS]
    }
    
    When the result is aggregtated we got:
    
    For resources:
    {
      "measures": { "aggregated": MEASURES},
      "references": [RESOURCES_DETAILS]
    }
    For metrics
    {
      "measures": { "aggregated": MEASURES},
      "references": [METRICS_DETAILS]
    }
    
    Closes-bug: #477

--- gnocchi/rest/api.py
+++ gnocchi/rest/api.py
@@ -1817,7 +1817,7 @@ class AggregationController(rest.RestController):
                     granularity, resample)
             return processor.get_measures(
                 pecan.request.storage,
-                [(m, aggregation) for m in metrics],
+                [processor.MetricReference(m, aggregation) for m in metrics],
                 operations, start, stop,
                 granularity, needed_overlap, fill)["aggregated"]
         except exceptions.UnAggregableTimeseries as e:
