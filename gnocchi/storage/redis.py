9211b7c18b65fff6ec52b7442101ee488e388344
Mehdi Abaakouk <sileht@redhat.com>
2017-11-29 16:41:31 +0100

    redis: add documentation for the url
    Closes #520

--- gnocchi/storage/redis.py
+++ gnocchi/storage/redis.py
@@ -13,18 +13,10 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from oslo_config import cfg
 
 from gnocchi.common import redis
 from gnocchi import storage
 from gnocchi import utils
-
-
-OPTS = [
-    cfg.StrOpt('redis_url',
-               default='redis://localhost:6379/',
-               help='Redis URL'),
-]
 
 
 class RedisStorage(storage.StorageDriver):
