866d2d9db3406ab52a64c05ad19460547dc1b8ff
Julien Danjou <julien@danjou.info>
2017-12-02 13:27:57 +0100

    indexer: do not return empty IN statement in QueryFilter
    Fixes #530
    
    (cherry picked from commit c89a0ef696a8bee5a95a336c599104c979c01b5f)

--- gnocchi/indexer/sqlalchemy.py
+++ gnocchi/indexer/sqlalchemy.py
@@ -1103,6 +1103,13 @@ class SQLAlchemyIndexer(indexer.IndexerDriver):
         return sort_keys, sort_dirs
 
 
+def _operator_in(field_name, value):
+    # Do not generate empty IN comparison
+    # https://github.com/gnocchixyz/gnocchi/issues/530
+    if len(value):
+        return field_name.in_(value)
+
+
 class QueryTransformer(object):
     unary_operators = {
         u"not": sqlalchemy.not_,
@@ -1131,7 +1138,7 @@ class QueryTransformer(object):
         u"≠": operator.ne,
         u"ne": operator.ne,
 
-        u"in": lambda field_name, values: field_name.in_(values),
+        u"in": _operator_in,
 
         u"like": lambda field, value: field.like(value),
     }
