2d9f83e522e5039aa635c694ac395656a878b0b3
Julien Danjou <julien@danjou.info>
2018-02-26 13:52:07 +0100

    storage: more batching of store/delete operations in add_measures
    Rather than emitting store and delete on each aggregation computing, this
    batches them all at once for a metric by returning the operations from
    `_add_measures` directly.

--- gnocchi/tests/test_carbonara.py
+++ gnocchi/tests/test_carbonara.py
@@ -398,7 +398,7 @@ class TestAggregatedTimeSerie(base.BaseTestCase):
 
     @staticmethod
     def _resample_and_merge(ts, agg_dict):
-        """Helper method that mimics _add_measures workflow."""
+        """Helper method that mimics _compute_splits_operations workflow."""
         grouped = ts.group_serie(agg_dict['sampling'])
         existing = agg_dict.get('return')
         agg_dict['return'] = carbonara.AggregatedTimeSerie.from_grouped_serie(
