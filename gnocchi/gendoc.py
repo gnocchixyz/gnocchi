c6d7fcb0c81fff208a0dcd33c7b2d9987f11d879
Mehdi Abaakouk <sileht@redhat.com>
2018-02-27 09:42:50 +0100

    doc: document filter parameter
    All resource search payload can also be passed in the filter parameter
    into other format. This is missing from the documentation while Grafana
    have to use this format. So documents it

--- gnocchi/gendoc.py
+++ gnocchi/gendoc.py
@@ -200,6 +200,10 @@ def setup(app):
 
     try:
         for entry in scenarios:
+            if 'filter' in entry:
+                entry['filter'] = jinja2.Template(entry['filter']).render(
+                    scenarios=scenarios)
+
             template = jinja2.Template(entry['request'])
             fake_file = six.moves.cStringIO()
             content = template.render(scenarios=scenarios)
