2f4cc4947675426a546b2dc20bf92a30509c1325
Mehdi Abaakouk <sileht@sileht.net>
2018-07-23 11:56:59 +0200

    Added amqp1.0 collectd daemon
    This enables gnocchi-amqp daemon.
    
    Which listens to metrics from collectd amqp1 write plugin.

--- doc/source/collectd.rst
+++ doc/source/collectd.rst
@@ -3,12 +3,45 @@
 ==================
 
 `Collectd`_ can use Gnocchi to store its data through a plugin called
-`collectd-gnocchi`. It can be installed with *pip*::
+`collectd-gnocchi` or via the `gnocchi-amqp1d` daemon.
+
+
+collectd-gnocchi
+================
+
+It can be installed with *pip*::
 
      pip install collectd-gnocchi
 
 `Sources and documentation`_ are also available.
 
 
+gnocchi-amqp1d
+==============
+
+You need first to setup the Collectd `amqp1 write plugin`::
+
+    <Plugin amqp1>
+      <Transport "name">
+        Host "localhost"
+        Port "5672"
+        Address "collectd"
+        <Instance "telemetry">
+            Format JSON
+        </Instance>
+      </Transport>
+    </Plugin>
+
+
+Then configure the AMQP 1.0 url in gnocchi.conf::
+
+    [amqp1d]
+    url = localhost:5672/u/collectd/telemetry
+
+
 .. _`Collectd`: https://www.collectd.org/
 .. _`Sources and documentation`: https://github.com/gnocchixyz/collectd-gnocchi
+.. _`amqp1 write plugin`: https://github.com/ajssmith/collectd/blob/d4cc32c4dddb01081c49a67d13ab4a737cda0ed0/src/collectd.conf.pod#plugin-amqp1
+.. TODO(sileht): Change the link when
+   https://collectd.org/documentation/manpages/collectd.conf.5.shtml will be
+   up2date
---
+++ gnocchi/tests/test_amqp1d.py
@@ -0,0 +1,97 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import datetime
+import json
+import uuid
+
+import mock
+import numpy
+
+from gnocchi import amqp1d
+from gnocchi.tests import base as tests_base
+from gnocchi import utils
+
+
+def datetime64(*args):
+    return numpy.datetime64(datetime.datetime(*args))
+
+
+class TestAmqp1d(tests_base.TestCase):
+
+    AMQP1D_USER_ID = str(uuid.uuid4())
+    AMQP1D_PROJECT_ID = str(uuid.uuid4())
+
+    def setUp(self):
+        super(TestAmqp1d, self).setUp()
+        self.conf.set_override("resource_type",
+                               "collectd_amqp1d", "amqp1d")
+        self.conf.set_override("creator",
+                               self.AMQP1D_USER_ID, "amqp1d")
+
+        self.index.create_archive_policy_rule("rule-amqp", "*", "medium")
+
+        self.server = amqp1d.AMQP1Server(self.conf)
+        self.server.processor.incoming = self.incoming
+        self.server.processor.indexer = self.index
+
+    @mock.patch.object(utils, 'utcnow')
+    def test_amqp1d(self, utcnow):
+        utcnow.return_value = utils.datetime_utc(2017, 1, 10, 13, 58, 36)
+
+        metrics = json.dumps([
+            {u'dstypes': [u'gauge'], u'plugin': u'memory', u'dsnames':
+             [u'value'], u'interval': 10.0, u'host': u'www.gnocchi.test.com',
+             u'values': [9], u'time': 1506712460.824, u'plugin_instance':
+             u'', u'type_instance': u'free', u'type': u'memory'},
+            {u'dstypes': [u'derive', u'derive'], u'plugin': u'interface',
+             u'dsnames': [u'rx', u'tx'], u'interval': 10.0, u'host':
+             u'www.gnocchi.test.com', u'values': [2, 5], u'time':
+             1506712460.824, u'plugin_instance': u'ens2f1', u'type_instance':
+             u'', u'type': u'if_errors'}
+        ])
+
+        self.server.on_message(mock.Mock(message=mock.Mock(body=metrics)))
+        self.server.processor.flush()
+
+        resources = self.index.list_resources(
+            self.conf.amqp1d.resource_type,
+            attribute_filter={"=": {"host": "www.gnocchi.test.com"}}
+        )
+        self.assertEqual(1, len(resources))
+        self.assertEqual("www.gnocchi.test.com",
+                         resources[0].host)
+
+        metrics = self.index.list_metrics(attribute_filter={
+            '=': {"resource_id": resources[0].id}
+        })
+        self.assertEqual(3, len(metrics))
+
+        self.trigger_processing(metrics)
+
+        expected_measures = {
+            "memory@memory-free": [
+                (datetime64(2017, 1, 10, 13, 58), numpy.timedelta64(1, 'm'), 9)
+            ],
+            "interface-ens2f1@if_errors-rx": [
+                (datetime64(2017, 1, 10, 13, 58), numpy.timedelta64(1, 'm'), 2)
+            ],
+            "interface-ens2f1@if_errors-tx": [
+                (datetime64(2017, 1, 10, 13, 58), numpy.timedelta64(1, 'm'), 5)
+            ]
+        }
+        for metric in metrics:
+            aggregation = metric.archive_policy.get_aggregation(
+                "mean", numpy.timedelta64(1, 'm'))
+            measures = self.storage.get_measures(metric, [aggregation])
+            self.assertEqual(expected_measures[metric.name],
+                             measures["mean"])
