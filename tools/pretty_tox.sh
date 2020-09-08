0230fb0b95b1a446dc3871f604182869522d8731
Tobias Urdin <tobias.urdin@binero.se>
2020-09-08 10:25:16 +0200

    Add back recommendations and remove debug
    Debug causes Travis to drop the job because
    there is to much output.

--- tools/pretty_tox.sh
+++ tools/pretty_tox.sh
@@ -10,7 +10,7 @@ TESTRARGS=$1
 #
 # this work around exists until that is addressed
 if [[ "$TESTARGS" =~ "until-failure" ]]; then
-    python3 setup.py testr --slowest --testr-args="$TESTRARGS"
+    python setup.py testr --slowest --testr-args="$TESTRARGS"
 else
-    python3 setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | subunit-trace -f
+    python setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | subunit-trace -f
 fi
