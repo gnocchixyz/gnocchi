f6a1525f2b557057e43ececc1f534166fc1ffccc
Takashi Kajinami <tkajinam@redhat.com>
2021-01-04 21:46:49 +0900

    Implement policy in code (2): Resources and Resource Types
    This change implements default policies for Resources API and Resource
    Types API in codes.

--- gnocchi/rest/policies.py
+++ gnocchi/rest/policies.py
@@ -22,6 +22,9 @@ RULE_RESOURCE_OWNER = "project_id:%(project_id)s"
 RULE_METRIC_OWNER = "project_id:%(resource.project_id)s"
 RULE_UNPROTECTED = ""
 
+ADMIN_OR_CREATOR = "rule:admin_or_creator"
+ADMIN_OR_CREATOR_OR_RESOURCE_OWNER = \
+    "rule:admin_or_creator or rule:resource_owner"
 ADMIN_OR_CREATOR_OR_METRIC_OWNER = "rule:admin_or_creator or rule:metric_owner"
 
 rules = [
@@ -43,6 +46,144 @@ rules = [
     )
 ]
 
+resource_rules = [
+    policy.DocumentedRuleDefault(
+        name="create resource",
+        check_str=RULE_UNPROTECTED,
+        description='Create a new resource.',
+        operations=[
+            {
+                'path': '/v1/resource/{resource_type}',
+                'method': 'POST'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="get resource",
+        check_str=ADMIN_OR_CREATOR_OR_RESOURCE_OWNER,
+        description='Get a resource.',
+        operations=[
+            {
+                'path': '/v1/resource/{resource_type}/{resource_id}',
+                'method': 'GET'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="update resource",
+        check_str=ADMIN_OR_CREATOR,
+        description='Update a resource.',
+        operations=[
+            {
+                'path': '/v1/resource/{resource_type}/{resource_id}',
+                'method': 'PATCH'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="delete resource",
+        check_str=ADMIN_OR_CREATOR,
+        description='Delete a resource.',
+        operations=[
+            {
+                'path': '/v1/resource/{resource_type}/{resource_id}',
+                'method': 'DELETE'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="delete resources",
+        check_str=ADMIN_OR_CREATOR,
+        description='Delete multiple resources.',
+        operations=[
+            {
+                'path': '/v1/resource/{resource_type}',
+                'method': 'DELETE'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="list resource",
+        check_str=ADMIN_OR_CREATOR_OR_RESOURCE_OWNER,
+        description='List all resources.',
+        operations=[
+            {
+                'path': '/v1/resource/{resource_type}',
+                'method': 'GET'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="search resource",
+        check_str=ADMIN_OR_CREATOR_OR_RESOURCE_OWNER,
+        description='Search resources.',
+        operations=[
+            {
+                'path': '/v1/search/resources/{resource_type}',
+                'method': 'POST'
+            }
+        ]
+    )
+]
+
+resource_type_rules = [
+    policy.DocumentedRuleDefault(
+        name="create resource type",
+        check_str=RULE_ADMIN,
+        description='Create a new resource type.',
+        operations=[
+            {
+                'path': '/v1/resource_type',
+                'method': 'POST'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="get resource type",
+        check_str=RULE_UNPROTECTED,
+        description='Get a resource type.',
+        operations=[
+            {
+                'path': '/v1/resource_type/{resource_type}',
+                'method': 'GET'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="update resource type",
+        check_str=RULE_ADMIN,
+        description='Update a resource type.',
+        operations=[
+            {
+                'path': '/v1/resource_type/{resource_type}',
+                'method': 'PATCH'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="delete resource type",
+        check_str=RULE_ADMIN,
+        description='Delete a resource type.',
+        operations=[
+            {
+                'path': '/v1/resource_type/{resource_type}',
+                'method': 'DELETE'
+            }
+        ]
+    ),
+    policy.DocumentedRuleDefault(
+        name="list resource type",
+        check_str=RULE_UNPROTECTED,
+        description='List all resource types.',
+        operations=[
+            {
+                'path': '/v1/resource_type',
+                'method': 'GET'
+            }
+        ]
+    )
+]
+
 
 def list_rules():
-    return rules
+    return rules + resource_rules + resource_type_rules
