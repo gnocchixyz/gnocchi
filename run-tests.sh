17457138ed89712f4dbcfdc4909aabc5a4e5cb40
Tobias Urdin <tobias.urdin@binero.com>
2021-07-01 16:55:41 +0200

    Use mkdir -p when creating npm-s3rver dir

--- run-tests.sh
+++ run-tests.sh
@@ -28,7 +28,7 @@ do
             s3)
                 if ! which s3rver >/dev/null 2>&1
                 then
-                    mkdir npm-s3rver
+                    mkdir -p npm-s3rver
                     export NPM_CONFIG_PREFIX=npm-s3rver
                     npm install s3rver --global
                     export PATH=$PWD/npm-s3rver/bin:$PATH
