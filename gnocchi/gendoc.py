fa7d13d495b605e1782dcef0df2cda7c723d1520
Thomas Goirand <zigo@debian.org>
2023-12-20 12:14:46 +0100

    Remove the use of the future module
    This module is buggy with Python 3.11 and 3.12, and is
    being actively removed from distributions (like Debian
    and Ubuntu). The absolute_import things are remaining
    of the past, in the Python 2.7 age. Let's get rid of them
    and remove future from setup.cfg.

--- gnocchi/gendoc.py
+++ gnocchi/gendoc.py
@@ -14,7 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from __future__ import absolute_import
 import io
 import json
 import os
--- gnocchi/indexer/sqlalchemy_base.py
+++ gnocchi/indexer/sqlalchemy_base.py
@@ -14,7 +14,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from __future__ import absolute_import
 
 from oslo_db.sqlalchemy import models
 import sqlalchemy
