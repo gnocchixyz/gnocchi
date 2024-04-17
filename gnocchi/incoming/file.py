728ef243efa8a089f0ba4c5a76367d0807c2cbda
Rafael Weingärtner <rafael@apache.org>
2024-04-17 13:31:52 -0300

    Fix Gnocchi manager upgrade command for File
    When running the upgrade command manuyally, we noticed that the "tmp"
    folder in the "conf.file_basepath" is not created, which will lead to an
    error later in the execution flow.

--- gnocchi/incoming/file.py
+++ gnocchi/incoming/file.py
@@ -35,6 +35,11 @@ class FileStorage(incoming.IncomingDriver):
         self.basepath = conf.file_basepath
         self.basepath_tmp = os.path.join(self.basepath, 'tmp')
 
+        # We need to make sure that the 'tmp' path exists; otherwise,
+        # we will have an issue when running the method "set_storage_settings"
+        # that created the temporary JSON file under the 'tmp' folder.
+        utils.ensure_paths([self.basepath_tmp])
+
     def __str__(self):
         return "%s: %s" % (self.__class__.__name__, str(self.basepath))
 
